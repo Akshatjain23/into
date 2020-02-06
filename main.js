e6bb0c10429d3d76b9b430afa4f3587229f2d6dc
Akshatjain23 <57037424+Akshatjain23@users.noreply.github.com>
2020-02-06 19:40:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,55 @@
 console.log('hello world');
-let age=5;
+const age=5;
 console.log(age);
+
+let score=8; 
+console.log(score);
+
+const name='akshat';
+const rating=5.8;I
+const a=true;
+const x=undefined;
+let q;
+
+console.log( 'my name is+name+ and i am +age+ old');
+console.log(my name is ${name} and i am ${age} old);
+s='hello world';
+console.log(s.touppercase());
+console.log(s.split(''));
+//array
+const a=[1,4,3,r,t,s];
+console.log(a);
+a.push('q');
+a.pop;
+console.log(array.isarray('4'));
+
+for(let i=0;i<11;i++);
+console.log(i);
+
+const x=10;
+if(x==10)
+{
+  console.log('x is 10');
+}
+
+function add(let k=1,let l=3);
+{
+console.log(k+l);
+}
+add(4,8);
+  
+  
+  
+  
+  
+  
+  
+  
+  
+              
+              
+              
+              
+              
+
+
