ba87874462f954a47a0bbd24449f6a2cb0b631e0
Akshatjain23 <57037424+Akshatjain23@users.noreply.github.com>
2020-02-21 23:48:49 +0530

    Create url.js

---
+++ url.js
@@ -0,0 +1,30 @@
+const express=require('express')
+const router=express.router();
+
+router.get('/name',(req,res,next)=>{
+rest.status(200).json({
+message:' hello/name'
+});
+});
+router.post('/',(req,res,next)=>{
+rest.status(200).json({
+message:' /name'
+});
+});
+router.get('/nameage',(req,res,next)=>
+{
+const age=req.params.page;
+if(age>='10'&&age<='18')
+{ res.status(200).json({
+message:'you are a teen'
+age:age
+});
+} else if(age<='10'){
+res.status(200).json({
+message:'you are a child'
+});
+
+else{
+(res.status(200).json({message:'you are an adult'});
+)};
+module.exports=router;
